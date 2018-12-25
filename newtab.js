1c9136c023751c1748bbf2c6200e579dea66a345
Faisal Alvi <faisalalvi33@gmail.com>
2018-12-25 12:08:12 +0530

    clear button added - save button removed!

--- newtab.js
+++ newtab.js
@@ -1,6 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-	//alert('hey');
-    //alert(localStorage['note1']);
     document.getElementById('note1').value = localStorage['note1'];
     document.getElementById('note2').value = localStorage['note2'];
     document.getElementById('note3').value = localStorage['note3'];
@@ -11,17 +9,8 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('note8').value = localStorage['note8'];
     document.getElementById('note9').value = localStorage['note9'];
 }, false);
-//alert(jQuery);
-// $("body").append('Test');
-
 
 document.addEventListener('DOMContentLoaded', function () {
-	// add event listener for buttons
-	//document.getElementById('open').addEventListener('click', loadSites);
-	//document.getElementById('extract').addEventListener('click', extractURLs);
-	document.getElementById('savenote').addEventListener('click', saveNote);
-	// focus on form field
-	//document.getElementById('urls').focus();
 	document.getElementById('note1').addEventListener('keydown', saveNote);
 	document.getElementById('note2').addEventListener('keydown', saveNote);
 	document.getElementById('note3').addEventListener('keydown', saveNote);
@@ -32,13 +21,39 @@ document.addEventListener('DOMContentLoaded', function () {
 	document.getElementById('note8').addEventListener('keydown', saveNote);
 	document.getElementById('note9').addEventListener('keydown', saveNote);
 
+	//document.getElementsByClassName('clear1').addEventListener('click', clearNote('clear1'));
+
+	document.getElementById("clear1").addEventListener("click", function(){
+		document.getElementById('note1').value = ''; localStorage['note1'] = '';
+	});
+	document.getElementById("clear2").addEventListener("click", function(){
+		document.getElementById('note2').value = ''; localStorage['note2'] = '';
+	});
+	document.getElementById("clear3").addEventListener("click", function(){
+		document.getElementById('note3').value = ''; localStorage['note3'] = '';
+	});
+	document.getElementById("clear4").addEventListener("click", function(){
+		document.getElementById('note4').value = ''; localStorage['note4'] = '';
+	});
+	document.getElementById("clear5").addEventListener("click", function(){
+		document.getElementById('note5').value = ''; localStorage['note5'] = '';
+	});
+	document.getElementById("clear6").addEventListener("click", function(){
+		document.getElementById('note6').value = ''; localStorage['note6'] = '';
+	});
+	document.getElementById("clear7").addEventListener("click", function(){
+		document.getElementById('note7').value = ''; localStorage['note7'] = '';
+	});
+	document.getElementById("clear8").addEventListener("click", function(){
+		document.getElementById('note8').value = ''; localStorage['note8'] = '';
+	});
+	document.getElementById("clear9").addEventListener("click", function(){
+		document.getElementById('note9').value = ''; localStorage['note9'] = '';
+	});
+
 });
 
-
-
-
 function saveNote () {
-    //alert('hey');
 	var note1 = document.getElementById('note1').value; localStorage['note1'] = note1;
 	var note2 = document.getElementById('note2').value; localStorage['note2'] = note2;
 	var note3 = document.getElementById('note3').value; localStorage['note3'] = note3;
@@ -48,8 +63,4 @@ function saveNote () {
 	var note7 = document.getElementById('note7').value; localStorage['note7'] = note7;
 	var note8 = document.getElementById('note8').value; localStorage['note8'] = note8;
 	var note9 = document.getElementById('note9').value; localStorage['note9'] = note9;
-	/*var note = chrome.storage.local.set({ 'note1' : '' }, function (){
-		//callback
-		alert('note saved successfully!');
-	});*/
 }
